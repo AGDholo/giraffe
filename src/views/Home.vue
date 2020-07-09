c331c600ece190d3edda7c086ead019fa5ba5c7a
Yan Lee <agdholo@gmail.com>
2020-07-09 08:19:06 +0800

    chore(card): add hover effort

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>
-      <v-card >
+      <v-card>
         <v-img
           src="https://cdn.pixabay.com/photo/2016/02/13/13/11/cuba-1197800_1280.jpg"
           gradient="to top, rgba(25,32,72,.7), rgba(25,32,72,.0)"
@@ -12,7 +12,7 @@
           <v-card-text class="fill-height d-flex align-end">
             <v-row class="flex-column">
               <v-col>
-                <v-btn color="accent" >Travel</v-btn>
+                <v-btn color="accent">Travel</v-btn>
               </v-col>
               <v-col cols="12" md="10" lg="8" xl="7">
                 <h2
@@ -41,41 +41,44 @@
 
             <v-row>
               <v-col cols="12" md="6" lg="4" v-for="i in 6" :key="i">
-                <v-card flat >
-                  <v-img
-                    src="https://cdn.pixabay.com/photo/2015/01/08/18/25/startup-593327_1280.jpg"
-                    :aspect-ratio="16 / 9"
-                    gradient="to top, rgba(25,32,72,.4), rgba(25,32,72,.0)"
-                    height="200px"
-                    class="elevation-2"
-                  >
-                    <v-card-text>
-                      <v-btn color="accent" >TIPS</v-btn>
-                    </v-card-text>
-                  </v-img>
-                </v-card>
+                <v-hover v-slot:default="{ hover }" open-delay="50" close-delay="50">
+                  <div>
+                    <v-card flat :color="hover ? 'white' : 'transparent'" :elevation="hover ? 12 : 0" hover>
+                      <v-img
+                        src="https://cdn.pixabay.com/photo/2015/01/08/18/25/startup-593327_1280.jpg"
+                        :aspect-ratio="16 / 9"
+                        gradient="to top, rgba(25,32,72,.4), rgba(25,32,72,.0)"
+                        height="200px"
+                        class="elevation-2"
+                        style="border-radius: 16px"
+                      >
+                        <v-card-text>
+                          <v-btn color="accent">TIPS</v-btn>
+                        </v-card-text>
+                      </v-img>
 
-                <v-card flat >
-                  <v-card-text>
-                    <div class="text-h5 font-weight-bold primary--text">
-                      How to write an awesome blog
-                      post in 5 steps
-                    </div>
+                      <v-card-text>
+                        <div class="text-h5 font-weight-bold primary--text">
+                          How to write an awesome blog
+                          post in 5 steps
+                        </div>
 
-                    <div class="text-body-1 py-4">
-                      Ultrices sagittis orci a scelerisque. Massa
-                      placerat duis ultricies lacus sed turpis
-                    </div>
+                        <div class="text-body-1 py-4">
+                          Ultrices sagittis orci a scelerisque. Massa
+                          placerat duis ultricies lacus sed turpis
+                        </div>
 
-                    <div class="d-flex align-center">
-                      <v-avatar color="accent" size="36">
-                        <v-icon dark>mdi-feather</v-icon>
-                      </v-avatar>
+                        <div class="d-flex align-center">
+                          <v-avatar color="accent" size="36">
+                            <v-icon dark>mdi-feather</v-icon>
+                          </v-avatar>
 
-                      <div class="pl-2">Yan Lee · 22 July 2019</div>
-                    </div>
-                  </v-card-text>
-                </v-card>
+                          <div class="pl-2">Yan Lee · 22 July 2019</div>
+                        </div>
+                      </v-card-text>
+                    </v-card>
+                  </div>
+                </v-hover>
               </v-col>
             </v-row>
           </div>
@@ -85,7 +88,7 @@
 
             <v-row>
               <v-col cols="6" lg="4" v-for="i in 3" :key="i">
-                <v-card flat  dark>
+                <v-card flat dark>
                   <v-img
                     src="https://cdn.pixabay.com/photo/2019/10/29/14/46/landscape-4587079_1280.jpg"
                     :aspect-ratio="16 / 9"
@@ -95,7 +98,7 @@
                   >
                     <div class="d-flex flex-column justify-space-between fill-height">
                       <v-card-text>
-                        <v-btn color="accent" >ANIMALS</v-btn>
+                        <v-btn color="accent">ANIMALS</v-btn>
                       </v-card-text>
 
                       <v-card-text>
@@ -125,7 +128,7 @@
             <div>
               <v-row v-for="i in 6" :key="i" class="py-4">
                 <v-col cols="12" md="4">
-                  <v-card flat  height="100%">
+                  <v-card flat height="100%">
                     <v-img
                       src="https://cdn.pixabay.com/photo/2020/07/04/06/34/mother-and-child-5368416_1280.jpg"
                       :aspect-ratio="16 / 9"
@@ -136,7 +139,7 @@
 
                 <v-col>
                   <div>
-                    <v-btn depressed  color="accent">TRAVEL</v-btn>
+                    <v-btn depressed color="accent">TRAVEL</v-btn>
 
                     <h3 class="text-h4 font-weight-bold pt-3">
                       Ut enim blandit volutpat maecenas volutpat
@@ -174,7 +177,7 @@
             <div>
               <v-row v-for="i in 5" :key="i" class="py-4">
                 <v-col cols="12" md="6" lg="5">
-                  <v-card height="100%"  flat>
+                  <v-card height="100%" flat>
                     <v-img
                       src="https://cdn.pixabay.com/photo/2020/06/30/22/34/dog-5357794_1280.jpg"
                       :aspect-ratio="16 / 9"
@@ -185,7 +188,7 @@
 
                 <v-col>
                   <div>
-                    <v-btn depressed  color="accent" small>TRAVEL</v-btn>
+                    <v-btn depressed color="accent" small>TRAVEL</v-btn>
 
                     <h3 class="text-h6 font-weight-bold primary--text py-3">
                       What do | need to know to
