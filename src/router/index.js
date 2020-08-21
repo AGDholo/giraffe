b5d2e7c47bf54ea8707dd17a2f7b8deb420c36e5
agdholo <agdholo@gmail.com>
2020-08-21 23:20:48 +0800

    fix(route): scroll to top on route change

--- src/router/index.js
+++ src/router/index.js
@@ -28,7 +28,10 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  scrollBehavior() {
+    document.getElementById('app').scrollIntoView();
+  }
 })
 
 export default router
