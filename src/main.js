1376a9698beba1deefedff170d326d50b33a6eb4
agdholo <agdholo@gmail.com>
2023-01-09 15:05:00 +0800

    chore: migrate vue entrypoint (2/2)

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ import "@mdi/font/css/materialdesignicons.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "./style.css";
+
+createApp(App).use(router).mount("#app");
