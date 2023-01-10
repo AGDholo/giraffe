bea7767eeb76e75609978f1948cfd762a49cb7dc
agdholo <agdholo@gmail.com>
2023-01-10 21:47:00 +0800

    chore: migrate router setup (2/5)

--- src/router/index.js
+++ src/router/index.js
@@ -6,3 +6,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/category",
+    name: "Category",
+    component: () => import("@/views/Category.vue"),
+  },
+  {
+    path: "/detail",
