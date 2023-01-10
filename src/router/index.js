4b77b7983a7b327557c84dbc48051dcd3700f148
agdholo <agdholo@gmail.com>
2023-01-10 21:18:00 +0800

    chore: migrate router setup (1/5)

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,8 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../views/Home.vue";
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/category',
-        name: 'Category',
-        component: () => import("@/views/Category.vue"),
-    },
-    {
-        path: '/detail',
-        name: 'Detail',
-        component: () => import("@/views/Detail.vue"),
-    },
-    {
-        path: '/authors',
-        name: 'Authors',
-        component: () => import("@/views/Authors.vue"),
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: () => import("@/views/Login.vue"),
-    }
-]
-
-const router = new VueRouter({
-    routes,
-    scrollBehavior() {
-        document.getElementById('app').scrollIntoView();
-    }
-})
-
-export default router
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
