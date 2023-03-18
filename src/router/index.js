164d513fe07831ed0be0b48f9bd8173e958b4216
agdholo <agdholo@gmail.com>
2023-03-19 00:47:00 +0800

    chore: migrate router setup (5/5)

--- src/router/index.js
+++ src/router/index.js
@@ -30,3 +30,11 @@ const routes = [
 ];
 
 const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+  scrollBehavior() {
+    return { top: 0 };
+  },
+});
+
+export default router;
