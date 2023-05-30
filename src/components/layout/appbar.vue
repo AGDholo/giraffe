9e7a25f7bcb818211bf75c6b575ee054db0fcf17
agdholo <agdholo@gmail.com>
2023-05-30 16:12:00 +0800

    refactor: migrate app bar (2/12)

--- src/components/layout/appbar.vue
+++ src/components/layout/appbar.vue
@@ -9,3 +9,15 @@
           <a
             v-for="(item, i) in btnItems"
             :key="`btn-${i}`"
+            :href="item.href"
+            :target="item.target"
+            class="block rounded px-3 py-3 text-base"
+            @click="drawer = false"
+          >
+            {{ item.text }}
+          </a>
+          <router-link
+            v-for="(item, i) in barItems"
+            :key="`bar-${i}`"
+            :to="item.to"
+            class="block rounded px-3 py-3 text-base"
