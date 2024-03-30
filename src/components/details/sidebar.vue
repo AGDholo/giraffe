6c97d0d88432f3543acdd2db62d697cb3322e175
agdholo <agdholo@gmail.com>
2024-03-31 00:26:00 +0800

    refactor: migrate sidebar (2/11)

--- src/components/details/sidebar.vue
+++ src/components/details/sidebar.vue
@@ -5,3 +5,11 @@
       <hr class="border-gray-200" />
 
       <div>
+        <div v-for="i in 5" :key="i" class="grid gap-4 py-2 md:grid-cols-[5fr_7fr] lg:grid-cols-[5fr_7fr]">
+          <div class="h-full">
+            <img
+              class="h-full min-h-28 w-full object-cover"
+              src="https://cdn.pixabay.com/photo/2021/01/22/16/55/camera-5940588_1280.jpg"
+              alt=""
+            />
+          </div>
