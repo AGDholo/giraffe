938b9d4da98f84ed5e1d50ec25e52fe608d8eb70
agdholo <agdholo@gmail.com>
2024-06-16 18:05:00 +0800

    refactor: migrate home view (2/13)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,3 +9,14 @@
           <div class="flex flex-col">
             <div>
               <router-link class="inline-flex rounded bg-accent px-4 py-2 text-white" to="/category">Travel</router-link>
+            </div>
+            <div class="w-full py-3 md:w-10/12 lg:w-8/12 xl:w-7/12">
+              <h2 class="text-5xl leading-tight">
+                Great Travel Blogs From Around The World To Inspire You
+              </h2>
+            </div>
+            <div class="flex items-center">
+              <span class="inline-flex h-12 w-12 items-center justify-center rounded-full bg-accent text-white shadow-lg">
+                <i class="mdi mdi-feather text-3xl"></i>
+              </span>
+              <div class="pl-2 text-xl">Yan Lee · 22 July 2019</div>
