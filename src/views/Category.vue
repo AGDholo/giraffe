f074a2be670c820a59b0bad617aeda3fdf6337c6
agdholo <agdholo@gmail.com>
2025-02-08 22:12:00 +0800

    refactor: migrate category view (2/8)

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -5,3 +5,10 @@
         <h2 class="text-4xl font-bold">ANIMAL</h2>
         <h4 class="text-xl">Some category description goes here</h4>
       </div>
+
+      <hr class="my-4 border-gray-200" />
+
+      <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <router-link
+          v-for="i in 18"
+          :key="i"
