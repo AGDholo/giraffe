cd425a77236b66a9b3f2c1f8a5452aad36c04761
agdholo <agdholo@gmail.com>
2025-02-10 00:33:00 +0800

    refactor: migrate category view (6/8)

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -34,3 +34,10 @@
               <span class="inline-flex h-9 w-9 items-center justify-center rounded-full bg-accent text-white">
                 <i class="mdi mdi-feather"></i>
               </span>
+              <div class="pl-2">Yan Lee · 22 July 2019</div>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
+
