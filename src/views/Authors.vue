440353b706183b31a6fba32f3f5e0b181286319d
agdholo <agdholo@gmail.com>
2025-04-21 23:26:00 +0800

    refactor: migrate authors view (2/8)

--- src/views/Authors.vue
+++ src/views/Authors.vue
@@ -4,3 +4,10 @@
       <div class="my-2">
         <h2 class="text-4xl font-bold">Our Authors</h2>
       </div>
+
+      <div class="grid gap-6 md:grid-cols-2">
+        <div
+          v-for="i in 8"
+          :key="i"
+          class="rounded transition-all duration-200 hover:bg-white hover:shadow-xl"
+        >
