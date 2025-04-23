9f3f16a13093b2bd550fe911ff8c93f8586314eb
agdholo <agdholo@gmail.com>
2025-04-23 15:26:00 +0800

    refactor: migrate authors view (5/8)

--- src/views/Authors.vue
+++ src/views/Authors.vue
@@ -24,3 +24,9 @@
             <div class="py-4 text-base">
               Ultrices sagittis orci a scelerisque. Massa placerat
               duis ultricies lacus sed turpis tincidunt id.
+            </div>
+            <div class="pb-4">
+              <button class="h-10 w-10 text-2xl" aria-label="Facebook"><i class="mdi mdi-facebook"></i></button>
+              <button class="h-10 w-10 text-2xl" aria-label="Twitter"><i class="mdi mdi-twitter"></i></button>
+              <button class="h-10 w-10 text-2xl" aria-label="YouTube"><i class="mdi mdi-youtube"></i></button>
+              <button class="h-10 w-10 text-2xl" aria-label="Instagram"><i class="mdi mdi-instagram"></i></button>
