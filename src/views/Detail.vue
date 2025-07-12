a346341e8e38f61039cfb764d2a97af2ce599b55
agdholo <agdholo@gmail.com>
2025-07-12 22:47:00 +0800

    refactor: migrate detail view (2/12)

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -13,3 +13,18 @@
         </div>
 
         <div class="pt-4 text-4xl font-bold text-primary">
+          <h4>15 Things | have always wondered about birds</h4>
+        </div>
+
+        <div class="py-4 text-base">
+          Mattis enim ut tellus elementum sagittis vitae et. Enim ut tellus elementum sagittis vitae et leo duis ut. Quis vel eros donec.
+          Faucibus nisl tincidunt eget nullam. Quis commodo odio aenean sed adipiscing diam doneg.
+        </div>
+
+        <div class="flex flex-col justify-between gap-4 sm:flex-row sm:items-center">
+          <div class="flex items-center">
+            <span class="inline-flex h-9 w-9 items-center justify-center rounded-full bg-accent text-white">
+              <i class="mdi mdi-feather"></i>
+            </span>
+            <div class="pl-2 text-base">Yan Lee · 22 July 2019</div>
+          </div>
