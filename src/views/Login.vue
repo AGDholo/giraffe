08bb752864883d3979d7e853aa3627bcc9e70885
agdholo <agdholo@gmail.com>
2026-01-24 20:54:00 +0800

    refactor: migrate login view (2/4)

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,3 +4,9 @@
       <div class="rounded bg-white py-6 shadow">
         <div class="flex justify-center">
           <div class="text-4xl">Login</div>
+        </div>
+        <div class="p-4">
+          <label class="mb-4 block">
+            <span class="mb-2 block text-sm text-secondary">Email</span>
+            <input class="h-12 w-full rounded border border-gray-300 px-4 outline-none focus:border-primary" />
+          </label>
